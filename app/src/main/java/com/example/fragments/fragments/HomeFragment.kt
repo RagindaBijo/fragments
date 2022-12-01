19ce448b8336de78878dd3a2464bf9f650a51370
Luka <luka.ivaniadze.2@btu.edu.ge>
2022-12-01 13:57:52 +0400

    first commit

---
+++ app/src/main/java/com/example/fragments/fragments/HomeFragment.kt
@@ -0,0 +1,9 @@
+package com.example.fragments.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.fragments.R
+
+class HomeFragment: Fragment(R.layout.fragment_home) {
+
+
+}
---
+++ app/src/main/java/com/example/fragments/fragments/PhotoFragment.kt
@@ -0,0 +1,14 @@
+package com.example.fragments.fragments
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.fragments.R
+
+class PhotoFragment: Fragment(R.layout.fragment_photo) {
+
+
+}
